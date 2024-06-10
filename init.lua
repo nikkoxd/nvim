cd21d583f83dfcc5eb31cd163c1617c8cc3b2099
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-06-10 18:19:54 +0400

    feat: initial commit

---
+++ init.lua
@@ -0,0 +1,24 @@
+require("remap")
+
+local lazypath = vim.fn.stdpath("data") .. "lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+require("lazy").setup("plugins")
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  "rebelot/kanagawa.nvim",
+
+  config = function()
+    require('kanagawa').setup({
+      transparent = true,
+    })
+
+    vim.cmd("colorscheme kanagawa")
+  end
+}
