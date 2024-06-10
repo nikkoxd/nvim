641df9a8baeb64146392a4af63a211921e80f123
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-06-10 21:12:06 +0400

    replace airline with lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.g.lualine_laststatus = vim.o.laststatus
+    if vim.fn.argc(-1) > 0 then
+      vim.o.statusline = " "
+    else
+      vim.o.laststatus = 0
+    end
+  end,
+  opts = function()
+    local lualine_require = require("lualine_require")
+    lualine_require.require = require
+
+    vim.o.laststatus = vim.g.lualine_laststatus
+
+    local opts = {
+      options = {
+        theme = "auto",
+        globalstatus = vim.o.laststatus == 3,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+      },
+    }
+
+    return opts
+  end
+}
