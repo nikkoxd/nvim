18159cb095c5f10d9abf7e17f3c628fae197545f
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-06-10 19:50:23 +0400

    configure fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,36 @@
 return {
-  "tpope/vim-fugitive"
+  "tpope/vim-fugitive",
+
+  config = function()
+    vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+    local FugitiveGroup = vim.api.nvim_create_augroup("FugitiveGroup", {})
+
+    local autocmd = vim.api.nvim_create_autocmd
+
+    autocmd("BufWinEnter", {
+      group = FugitiveGroup,
+      pattern = "*",
+      callback = function ()
+        if vim.bo.ft ~= "fugitive" then
+          return
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = {buffer = bufnr, remap = false}
+        vim.keymap.set("n", "<leader>p", function()
+          vim.cmd.Git("push")
+        end, opts)
+
+        vim.keymap.set("n", "<leader>P", function()
+          vim.cmd.Git({"pull", "--rebase"})
+        end, opts)
+
+        vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+      end
+    })
+
+    vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
+    vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+  end
 }
