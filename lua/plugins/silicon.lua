81ff581ff8ae49c30a5e3d72781361fa3b15b55f
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-06-14 05:03:32 +0400

    fix: change silicon theme

--- lua/plugins/silicon.lua
+++ lua/plugins/silicon.lua
@@ -6,12 +6,13 @@ return {
   opts = {
     disable_defaults = true,
     font = "MesloLGS Nerd Font Mono",
-    theme = "OneHalfLight",
+    theme = "Coldark-Cold",
     background = nil,
     pad_horiz = 0,
     pad_vert = 0,
     no_round_corner = true,
     no_window_controls = true,
+    no_line_number = true,
     language = function()
       return vim.bo.filetype
     end,
