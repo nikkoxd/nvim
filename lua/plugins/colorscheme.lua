c74f06cb050037a28557bfc54c20c3cd6850bb40
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-06-14 05:02:19 +0400

    fix: lazy load the colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,41 +1,39 @@
 return {
   "rebelot/kanagawa.nvim",
 
-  config = function()
-    require('kanagawa').setup({
-      transparent = true,
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none"
-            }
+  lazy = true,
+
+  opts = {
+    transparent = true,
+    colors = {
+      theme = {
+        all = {
+          ui = {
+            bg_gutter = "none"
           }
         }
-      },
-      overrides = function(colors)
-        local theme = colors.theme
-        return {
-          NormalFloat = { bg = "none" },
-          FloatBorder = { bg = "none" },
-          Floattitle = { bg = "none" },
+      }
+    },
+    overrides = function(colors)
+      local theme = colors.theme
+      return {
+        NormalFloat = { bg = "none" },
+        FloatBorder = { bg = "none" },
+        Floattitle = { bg = "none" },
 
-          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
 
-          LazyNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
-          MasonNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+        LazyNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+        MasonNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
 
-          TelescopeTitle = { fg = theme.ui.special, bold = true },
-          TelescopePromptNormal = { bg = theme.ui.bg_p1 },
-          TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
-          TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
-          TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
-          TelescopePreviewNormal = { bg = theme.ui.bg_dim },
-          TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
-        }
-      end
-    })
-
-    vim.cmd("colorscheme kanagawa")
-  end
+        TelescopeTitle = { fg = theme.ui.special, bold = true },
+        TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+        TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+        TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+        TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+        TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+        TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+      }
+    end,
+  }
 }
