8d9c9ba9f3e1d2f3667f62836f06ac3e33b24539
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-06-14 05:32:41 +0400

    fix: pass config as opts

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,12 +1,10 @@
 return {
   "nvim-lualine/lualine.nvim",
 
-  config = function()
-    require("lualine").setup({
-      options = {
-        section_separators = "",
-        component_separators = "",
-      }
-    })
-  end
+  opts = {
+    options = {
+      section_separators = "",
+      component_separators = "",
+    }
+  }
 }
