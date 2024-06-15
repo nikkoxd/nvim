af372572afee38a216621a1079c0b90cdb2fc7a3
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-06-15 17:55:26 +0400

    feat: add astro syntax highlighting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "rust" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "rust", "astro" },
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
