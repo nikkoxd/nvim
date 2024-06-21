ccb20c6283b20722b2748ff568c311a9bace9958
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-06-22 02:29:39 +0400

    fix: move keymaps to keys

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,18 +1,13 @@
 return {
-  'nvim-telescope/telescope.nvim', 
+  'nvim-telescope/telescope.nvim',
   tag = '0.1.6',
 
   dependencies = { 'nvim-lua/plenary.nvim' },
 
-  config = function()
-    require('telescope').setup({})
-
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-    vim.keymap.set('n', '<leader>ps', function()
-      builtin.grep_string({search = vim.fn.input('Grep > ') })
-    end)
-    vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-  end
+  keys = {
+    { '<leader>pf', function() require('telescope.builtin').find_files() end, desc = 'Find files' },
+    { '<C-p>', function() require('telescope.builtin').git_files() end, desc = 'Git files' },
+    { '<leader>ps', function() require('telescope.builtin').grep_string({search = vim.fn.input('Grep > ') }) end, desc = 'Grep string' },
+    { '<leader>vh', function() require('telescope.builtin').help_tags() end, desc = 'Help tags' },
+  },
 }
