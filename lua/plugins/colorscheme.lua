df0f472a6e53b31cffced087958c477a86106980
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-06-29 23:31:33 +0400

    fix: set colorscheme to rose-pine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,6 +1,12 @@
 return {
-  "uZer/pywal16.nvim",
-  name = "pywal16",
-
-  opts = {}
+  "rose-pine/neovim",
+  name = "rose-pine",
+  config = function()
+    require("rose-pine").setup({
+      styles = {
+        transparency = true,
+      }
+    })
+    vim.cmd("colorscheme rose-pine")
+  end
 }
