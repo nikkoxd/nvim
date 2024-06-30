4b18904534a8490b4a48fb9eed91ea090154200f
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-01 03:55:14 +0400

    feat: add fancy diagnostic messages

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,8 +8,6 @@ return {
   },
 
   config = function()
-    local cmp_nvim_lsp = require("cmp_nvim_lsp");
-    local capabilities = cmp_nvim_lsp.default_capabilities()
     vim.api.nvim_create_autocmd("LspAttach", {
       group = vim.api.nvim_create_augroup("UserLspConfig", {}),
       callback = function(ev)
