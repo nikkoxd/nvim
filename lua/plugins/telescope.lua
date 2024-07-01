082f131b55e4ff1623a98759faef6ac18f8a83d3
nikko <35370166+nikkoxd@users.noreply.github.com>
2024-07-01 04:04:07 +0400

    fix: use live grep to grep string

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,7 +7,7 @@ return {
   keys = {
     { '<leader>pf', function() require('telescope.builtin').find_files() end, desc = 'Find files' },
     { '<C-p>', function() require('telescope.builtin').git_files() end, desc = 'Git files' },
-    { '<leader>ps', function() require('telescope.builtin').grep_string({search = vim.fn.input('Grep > ') }) end, desc = 'Grep string' },
+    { '<leader>ps', function() require('telescope.builtin').live_grep() end, desc = 'Grep string' },
     { '<leader>vh', function() require('telescope.builtin').help_tags() end, desc = 'Help tags' },
   },
 
