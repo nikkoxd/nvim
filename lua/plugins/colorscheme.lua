9ecc4967d26b5808d94bbc6aa21602cc0be00798
nikko <35370166+nikkoxd@users.noreply.github.com>
2026-03-03 01:30:35 +0400

    Fix neopywal

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,46 +7,50 @@ if vim.fn.has('macunix') == 1 then
     keys = {
       { "<leader>nc", "<cmd>NeopywalCompile<cr>", desc = "Recompile neopywal" },
     },
-    opts = {
-      transparent_background = vars.transparency,
-      show_split_lines = false,
-      dim_inactive = false,
-      show_end_of_buffer = true,
-      custom_highlights = function(C)
-        return {
-          all = {
-            StatusLine = { bg = C.none },
+    config = function()
+      require('neopywal').setup({
+        transparent_background = true,
+        show_split_lines = false,
+        dim_inactive = false,
+        show_end_of_buffer = true,
+        custom_highlights = function(C)
+          return {
+            all = {
+              StatusLine = { bg = C.none },
 
-            lualine_lsp = { bg = C.none, fg = C.color8 },
-            lualine_icon_branch = { bg = C.none, fg = C.color3 },
+              lualine_lsp = { bg = C.none, fg = C.color8 },
+              lualine_icon_branch = { bg = C.none, fg = C.color3 },
 
-            NormalFloat = { bg = C.background },
-            FloatBorder = { bg = C.background, fg = C.foreground },
-            FloatTitle = { bg = C.background, fg = C.color4 },
+              NormalFloat = { bg = C.background },
+              FloatBorder = { bg = C.background, fg = C.foreground },
+              FloatTitle = { bg = C.background, fg = C.color4 },
 
-            MiniFilesTitle = { bg = C.background, fg = C.color8 },
-            MiniFilesBorderModified = { bg = C.background, fg = C.color3 },
+              MiniFilesTitle = { bg = C.background, fg = C.color8 },
+              MiniFilesBorderModified = { bg = C.background, fg = C.color3 },
 
-            WhichKey = { bg = C.background },
+              WhichKey = { bg = C.background },
 
-            EndOfBuffer = { fg = C.background },
-            ColorColumn = { bg = C.background },
+              EndOfBuffer = { fg = C.background },
+              ColorColumn = { bg = C.background },
 
-            Pmenu = { bg = C.background },
-            PmenuSel = { bg = C.color4, fg = C.color0 },
+              Pmenu = { bg = C.background },
+              PmenuSel = { bg = C.color4, fg = C.color0 },
 
-            CmpItemAbbrDeprecated = { fg = C.color7, strikethrough = true },
-            CmpItemMenu = { fg = C.color7, italic = true },
-          },
-        }
-      end,
-      default_plugins = false,
-      plugins = {
-        gitsigns = true,
-        harpoon = true,
-        trouble = true,
-      },
-    },
+              CmpItemAbbrDeprecated = { fg = C.color7, strikethrough = true },
+              CmpItemMenu = { fg = C.color7, italic = true },
+            }
+          }
+        end,
+        default_plugins = false,
+        plugins = {
+          gitsigns = true,
+          harpoon = true,
+          trouble = true,
+        },
+      })
+
+      vim.cmd [[colorscheme neopywal]]
+    end
   }
 else
   return {
