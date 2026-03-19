6ca6f53b20e2db641b48bfc29ee272ef3a0af2e3
Владислав <vladik.nekrasov3@gmail.com>
2026-03-19 12:10:05 +0400

    Update stuff

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,16 +8,33 @@ return {
 		},
 		sections = {
 			lualine_a = {
-				{ "mode", separator = { left = "", right = "" } },
+				{
+					"mode",
+					separator = { left = "", right = "" },
+					---@param str string
+					fmt = function(str)
+						return str:sub(0, 3)
+					end,
+				},
 			},
 			lualine_b = {
-				{ "branch", icon = "󰘬", padding = { left = 2, right = 1 } },
+				{
+					"branch",
+					icon = "󰘬",
+					padding = { left = 2, right = 1 },
+				},
 			},
-      lualine_y = {
-				{ "progress", padding = { left = 1, right = 2 } },
-      },
+			lualine_y = {
+				{
+					"progress",
+					padding = { left = 1, right = 2 },
+				},
+			},
 			lualine_z = {
-				{ "location", separator = { left = "", right = "" } },
+				{
+					"location",
+					separator = { left = "", right = "" },
+				},
 			},
 		},
 	},
--- lua/plugins/notes.lua
+++ lua/plugins/notes.lua
@@ -20,6 +20,7 @@ return {
   },
   {
     "OXY2DEV/markview.nvim",
+    enabled = false,
     lazy = false,
 
     dependencies = { "saghen/blink.cmp" },
